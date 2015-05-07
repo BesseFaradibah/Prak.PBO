67f4ebdf1bf46ae99f0387de7166d2ac37bec49b
bessefaradibah <60200112060@uin-alauddin.ac.id>
2015-05-07 13:40:23 -0700

    versi 2

---
+++ src/praktikum2/Latihan1.java
@@ -0,0 +1,27 @@
+package praktikum2;
+
+public class Latihan1 {
+	static boolean bool;
+	static byte by;
+	static char ch;
+	static double d;
+	static float f;
+	static int i;
+	static long l;
+	static short sh;
+	static String str;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("byte    = "+ by);
+		System.out.println("char    = "+ ch);
+		System.out.println("double  = "+ d);
+		System.out.println("float   = "+ f);
+		System.out.println("integer = "+ i);
+		System.out.println("long    = "+ l);
+		System.out.println("short   = "+ sh);
+		System.out.println("string  = "+ str);
+	}
+
+}
---
+++ src/praktikum3/Latihan1.java
@@ -0,0 +1,34 @@
+package praktikum3;
+
+public class Latihan1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int hasil = 1 + 2;
+	System.out.println(hasil);
+	
+	hasil = hasil - 1;
+	System.out.println(hasil);
+	
+	hasil = hasil * 2;
+	System.out.println(hasil);
+	
+	hasil = hasil / 2;
+	System.out.println(hasil);
+	
+	hasil = hasil + 8;
+	hasil = hasil % 7;
+	System.out.println(hasil);
+	
+	hasil += 1;
+	System.out.println(hasil);
+	
+	hasil -= 2;
+	System.out.println(hasil);
+	
+	hasil *= 4;
+	System.out.println(hasil);
+	}
+
+}
